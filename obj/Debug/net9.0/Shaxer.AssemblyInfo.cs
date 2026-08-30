16b4fe46899551df5762eb622eeb3e3dac76a730
IsaacFeld <isaacbfeldman@gmail.com>
2026-08-30 10:52:05 +0200

    idk

--- obj/Debug/net9.0/Shaxer.AssemblyInfo.cs
+++ obj/Debug/net9.0/Shaxer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shaxer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5d8504162a4446bf5ec0baf4e72473202a8d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49572d146974a4b7ae92ec3f93331c39a4cd260")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shaxer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shaxer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
